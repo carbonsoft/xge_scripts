0952464904703ffd3fdaf4b4360b0dda0e6df3d0
Strizhechenko Oleg <oleg@carbonsoft.ru>
2014-02-08 21:28:16 +0600

    coa:	add disconnect request support

--- usr/local/bin/coa.sh
+++ usr/local/bin/coa.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+PACKET_TYPE="$1"
+shift
+
 PARAMS="$@"
 PARAMS=${PARAMS//Framed-IP-Address/$FRAMED_IP_ADDRESS}
 n=$'\n'
-LOGFILE="/var/log/xge/coa.log"
+LOGFILE="/var/log/xge/sessions.log"
 [ ! -d /var/log/xge/ ] && mkdir -p /var/log/xge/
-echo "$0[$$]: $PARAMS" >> $LOGFILE
+echo "$0[$$]: $PACKET_TYPE $PARAMS" >> $LOGFILE
+
+if [ "$PACKET_TYPE" = 'Disconnect-Request' ]; then
+	PARAMS="session disconnect $FRAMED_IP_ADDRESS"
+fi
+
 REPLY="$( . /usr/local/bin/xgesh $PARAMS 2>&1 )"
 ERR=$?
 REPLY="${REPLY//$n/ }"
 REPLY="${REPLY//  / }"
 REPLY="${REPLY//\"/}"
-echo -e "Reply-Message = \"$REPLY (ERR: $ERR)\""
+# echo -e "Reply-Message = \"$REPLY (ERR: $ERR)\""
+# REPLY="${REPLY//[^a-zA-Z0-9_. ]/ }"
+echo -e "Reply-Message = \"${REPLY:0:1010} (ERR: $ERR)\""
 echo "$0[$$]: $@ REPLY: $REPLY (ERR: $ERR)" >> $LOGFILE
 exit 0
