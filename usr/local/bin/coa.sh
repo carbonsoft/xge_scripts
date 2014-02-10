d38d118ed49318b2ba9a347e3d3db59b927d95a9
Strizhechenko Oleg <oleg@carbonsoft.ru>
2014-02-10 14:56:08 +0600

    coa.sh:	поддержка многострочных атрибутов

--- usr/local/bin/coa.sh
+++ usr/local/bin/coa.sh
@@ -2,8 +2,8 @@
 
 PACKET_TYPE="$1"
 shift
-
 PARAMS="$@"
+
 PARAMS=${PARAMS//Framed-IP-Address/$FRAMED_IP_ADDRESS}
 n=$'\n'
 LOGFILE="/var/log/xge/sessions.log"
@@ -14,13 +14,26 @@ if [ "$PACKET_TYPE" = 'Disconnect-Request' ]; then
 	PARAMS="session disconnect $FRAMED_IP_ADDRESS"
 fi
 
-REPLY="$( . /usr/local/bin/xgesh $PARAMS 2>&1 )"
+REPLY="$( . /usr/local/bin/xgesh $PARAMS 2>&1)"
 ERR=$?
-REPLY="${REPLY//$n/ }"
-REPLY="${REPLY//  / }"
 REPLY="${REPLY//\"/}"
-# echo -e "Reply-Message = \"$REPLY (ERR: $ERR)\""
-# REPLY="${REPLY//[^a-zA-Z0-9_. ]/ }"
-echo -e "Reply-Message = \"${REPLY:0:1010} (ERR: $ERR)\""
+
+while read line; do
+    echo "Reply-Message += \"$line\","
+done <<< "$REPLY"
+echo "Error-Cause = $((200+ERR))"
 echo "$0[$$]: $@ REPLY: $REPLY (ERR: $ERR)" >> $LOGFILE
-exit 0
+exit $ERR
+# todo
+#  < 0 : fail      the module failed
+#  = 0 : okthe module succeeded
+#  = 1 : reject    the module rejected the user
+#  = 2 : fail      the module failed
+#  = 3 : okthe module succeeded
+#  = 4 : handled   the module has done everything to handle the request
+#  = 5 : invalid   the user's configuration entry was invalid
+#  = 6 : userlock  the user was locked out
+#  = 7 : notfound  the user was not found
+#  = 8 : noop      the module did nothing
+#  = 9 : updated   the module updated information in the request
+#  > 9 : fail      the module failed
