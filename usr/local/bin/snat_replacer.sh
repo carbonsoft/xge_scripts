b30b3d5c0df9ddce2195fb2816fd8ac3086ffeba
e.pimenov <e.pimenov@carbonsoft.ru>
2015-02-09 12:01:02 +0500

    fix snat_replaser

--- usr/local/bin/snat_replacer.sh
+++ usr/local/bin/snat_replacer.sh
@@ -5,13 +5,10 @@
 main() {
 	[ "${reservation:-0}" != "1" ] && return 0
 
-	timeout=$1
-	__log start snat_replacer with timeout $timeout
-	while sleep $timeout; do
-		__log snat_replacer new loop
-		for channel in $channels_list; do
-			check_and_fix_channel $channel
-		done
+	__log start snat_replacer with
+	for channel in $channels_list; do
+		__log check_and_fix_channel $channel
+		check_and_fix_channel $channel
 	done
 }
 
