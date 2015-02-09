3599610ab3e3f6209e3263abe8e3e60d6a0d1ccf
e.pimenov <e.pimenov@carbonsoft.ru>
2015-02-09 12:51:31 +0500

    в crontab добавлен snat_replacer

--- usr/local/bin/snat_replacer.sh
+++ usr/local/bin/snat_replacer.sh
@@ -3,9 +3,12 @@
 . /usr/local/lib/carbon_channels_lib
 
 main() {
-	[ "${reservation:-0}" != "1" ] && return 0
+	if [ "${reservation:-0}" != "1" ]; then
+		__log reservation is off - skip
+		return 0
+	fi
 
-	__log start snat_replacer with
+	__log start snat_replacer
 	for channel in $channels_list; do
 		__log check_and_fix_channel $channel
 		check_and_fix_channel $channel
